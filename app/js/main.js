12d6782d7e03b45dbb0eae3faf2ed1a735a20fb5
Chirkovv Artem <patyomkin2011@mail.ru>
2021-09-08 11:22:25 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,13 @@ $ (function (){
     dots: true,
     arrows: false,
     fade: true
-  })
+  });
+
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: "true",
+  });
 
 });
